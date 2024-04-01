95c0089747436abd613d9e6ff3bc5bf6de77d6ad
Cloud User <ec2-user@ip-172-31-30-89.ec2.internal>
2024-04-01 09:10:10 +0000

    changes the RHEL-7.9 scripts

--- tarball/CentOS-RHEL7.9_Kernel3.10_glibc2.17      /tarball_create.sh
+++ tarball/CentOS-RHEL7.9_Kernel3.10_glibc2.17      /tarball_create.sh
@@ -1,7 +1,67 @@
 #!/bin/bash
 
+# Function to check Node.js version
+check_node_version() {
+  if ! command -v node &> /dev/null; then
+    echo "Node.js is not installed."
+    return 1
+  fi
+
+  NODE_VERSION=$(node -v | grep -oP '^v\K16')
+  if [ "$NODE_VERSION" != "16" ]; then
+    echo "Node.js version 16 is required."
+    return 1
+  fi
+}
+
+# Function to check Yarn version
+check_yarn_version() {
+  if ! command -v yarn &> /dev/null; then
+    echo "Yarn is not installed."
+    return 1
+  fi
+
+  # Since yarn doesn't follow semantic versioning in its version command,
+  # and it's challenging to enforce a specific version without knowing how it's used,
+  # we just check if it's installed.
+}
+
+# Function to check Go version
+check_go_version() {
+  if ! command -v go &> /dev/null; then
+    echo "Go is not installed."
+    return 1
+  fi
+
+  GO_VERSION=$(go version | grep -oP 'go1\.20\.6')
+  if [ "$GO_VERSION" != "go1.20.6" ]; then
+    echo "Go version 1.20.6 is required."
+    return 1
+  fi
+}
+
+# Check all versions
+check_node_version && check_yarn_version && check_go_version
+
+if [ $? -eq 0 ]; then
+  echo "All required versions of the Go, Node and Yarn are installed. We are good to go."
+else
+  echo "Required versions and dependencies of Go, Node and Yarn are not installed."
+  exit 1
+fi
+
+
 export CGO_ENABLED=1
-make deps
+
+if [ -f yarn.lock ]; then
+  rm yarn.lock
+  echo "yarn.lock file was found and has been removed for the purpose of building the tarball. It will be re-generated"
+else
+  echo "yarn.lock file does not exist."
+fi
+
+
+make rumi-fe
 make build
 
 mkdir nvx-rumi-monitor-10.1.1-1/
